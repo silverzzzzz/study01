719f6d354ad4e2fc89b8526b28089cee49719b10
Fedoraジン <fedorazin12@gmail.com>
2020-11-29 01:01:40 +0900

    no message

---
+++ search.py
@@ -0,0 +1,19 @@
+# coding: UTF-8
+### 検索ツールサンプル
+### これをベースに課題の内容を追記してください
+
+# 検索ソース
+source=["ねずこ","たんじろう","きょうじゅろう","ぎゆう","げんや","かなお","ぜんいつ"]
+
+### 検索ツール
+def search():
+    word = input("鬼滅の登場人物の名前を入力してください >>> ")
+    ### ここに検索ロジックを書く
+    flag = word in source
+    if flag == 1:
+        print("{}が見つかりました".format(word))
+    else:
+        print("{}は見つかりませんでした".format(word))
+
+if __name__ == "__main__":
+    search()
